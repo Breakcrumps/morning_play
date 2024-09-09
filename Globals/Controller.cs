f8454eb33afe55c7f4bdb9327676c2a31dc92cdd
Breakcrumps <sonickita7@gmail.com>
2024-09-09 13:38:02 +0300

    Added acceleration logic and formatted code to fit conventions.

--- Globals/Controller.cs
+++ Globals/Controller.cs
@@ -1,19 +1,23 @@
 using static Godot.Input;
 using Godot;
 
+namespace Morning_Play.Globals {
 
-public partial class Controller : Node2D {
+  public partial class Controller : Node2D {
 
-  public Vector2 Velocity => new(DirX(), DirY());
+    public static Vector2 Velocity => new(DirX(), DirY());
 
-  static int DirX() {
-    if (IsActionPressed("Left") && !IsActionPressed("Right"))  return -1;
-    if (IsActionPressed("Right") && !IsActionPressed("Left"))  return 1;
-    return 0;
+    private static int DirX() {
+      if (IsActionPressed("Left") && !IsActionPressed("Right"))  return -1;
+      if (IsActionPressed("Right") && !IsActionPressed("Left"))  return 1;
+      return 0;
+    }
+    private static int DirY() {
+      if (IsActionPressed("Up") && !IsActionPressed("Down"))  return -1;
+      if (IsActionPressed("Down") && !IsActionPressed("Up"))  return 1;
+      return 0;
+    }
+  
   }
-  static int DirY() {
-    if (IsActionPressed("Up") && !IsActionPressed("Down"))  return -1;
-    if (IsActionPressed("Down") && !IsActionPressed("Up"))  return 1;
-    return 0;
-  }
+
 }
--- MC/Mc.cs
+++ MC/Mc.cs
@@ -1,26 +1,38 @@
 using Godot;
-
+using Morning_Play.Globals;
 
 namespace Morning_Play.MC {
 
   public partial class Mc : CharacterBody2D {
+    
+    private static float AccelerationTime => 20;
+    private static float Speed { get; set; } = 0;
+    private static float MaxSpeed => 50;
 
-    static int Speed => 20;
-
-    Controller Controller => GetNode<Controller>("Controller");
-    AnimationPlayer Player => GetNode<AnimationPlayer>("AnimationPlayer");
+    private Controller Controller => GetNode<Controller>("Controller");
+    private AnimationPlayer Player => GetNode<AnimationPlayer>("AnimationPlayer");
 
     public override void _PhysicsProcess(double delta) {
       ApplyVelocity(Controller.Velocity);
       MoveAndSlide();
     }
 
-    void ApplyVelocity(Vector2 velocity) {
+    private void ApplyVelocity(Vector2 velocity) {
+
       if (velocity == Vector2.Zero)
         Player.Play("Idle");
-      else
+      else {
         Player.Play("Walk");
-      Velocity = velocity * Speed;
+        if (Speed < MaxSpeed)
+          Speed += MaxSpeed / AccelerationTime;
+        else
+          Speed = MaxSpeed;
+      }
+
+      Velocity = velocity.Normalized() * Speed;
+      
     }
+  
   }
+
 }
