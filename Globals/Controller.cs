57edff1f5633b584e7da68aa0ff5ad12a2db6976
Breakcrumps <sonickita7@gmail.com>
2024-09-09 13:01:57 +0300

    Made methods static when possible for optimisation and code agility.

--- Globals/Controller.cs
+++ Globals/Controller.cs
@@ -6,12 +6,12 @@ public partial class Controller : Node2D {
 
   public Vector2 Velocity => new(DirX(), DirY());
 
-  int DirX() {
+  static int DirX() {
     if (IsActionPressed("Left") && !IsActionPressed("Right"))  return -1;
     if (IsActionPressed("Right") && !IsActionPressed("Left"))  return 1;
     return 0;
   }
-  int DirY() {
+  static int DirY() {
     if (IsActionPressed("Up") && !IsActionPressed("Down"))  return -1;
     if (IsActionPressed("Down") && !IsActionPressed("Up"))  return 1;
     return 0;
