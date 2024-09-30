ec932491636b38d9f2a333f8d8b0af269e7d563c
Breakcrumps <sonickita7@gmail.com>
2024-09-30 14:33:16 +0300

    Made way back I.

--- Scripts/Support/SceneTransition.cs
+++ Scripts/Support/SceneTransition.cs
@@ -1,16 +1,20 @@
 ﻿public partial class SceneTransition : Area2D {
 
-  [Export]
-  private PackedScene _scene;
+  [Export(PropertyHint.File, "*.tscn")]
+  private string _scenePath;
 
   public override void _Ready() {
     BodyEntered += TransitionScene;
   }
 
   private void TransitionScene(Node2D body) {
+
     if (body is not Player)
       return;
-    GetTree().CallDeferred("change_scene_to_packed", _scene);
+
+    var scene = ResourceLoader.Load<PackedScene>(_scenePath);
+    GetTree().CallDeferred("change_scene_to_packed", scene);
+    
   }
 
 }
